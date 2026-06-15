a3e3c95347b31e8fdb28fda68834ffa94b6c86d1
hjh <xiaodu4346@foxmail.com>
2026-06-15 22:18:05 +0800

    写了bubbleSort

--- 6.15-test.cpp
+++ 6.15-test.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-using namespace std;
+void bubbleSort(vector<int>& array) {
+    for (int i = 0; i < array.size(); i++) {
+        for (int j = 0; j < array.size() - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+    
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    //实现冒泡排序
+    vector<int>array1;
+    array1.push_back(20);
+    array1.push_back(123);
+    array1.push_back(415);
+    array1.push_back(257);
+    array1.push_back(398);
+    array1.push_back(234);
 
-    // Write your algorithm practice code here.
-    cout << "Hello, algorithm practice!" << '\n';
-
-    return 0;
+    bubbleSort(array1);
+    for (int i = 0; i < array1.size(); i++) {
+        cout << array1[i] << endl;
+    }
 }
